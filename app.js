ca4c737de5bf3bf79567b2064e3f38242924da0f
Mchrispin <mchrispin14@gmail.com>
2025-01-16 13:34:59 +0200

    added

---
+++ app.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./static/swagger.json') // Swagger JSON documentation file
+const { connectDB, disconnectDB } = require('./config/db'); // Import connect and disconnect
+
+const app = express()
+
+// Middleware
+app.use(express.json())
+
+// Serve Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+connectDB(); // Connecting to MongoDB
+
+app.get('/', (req, res)=>{
+    res.send('Welcome to the Library Management System API')
+})
+
+// Gracefully shut down the server on SIGINT signal (ctrl+c)
+process.on('SIGINT', async () => {
+    console.log('Received SIGINT. Closing MongoDB connection...\nServer stopped successfully.');
+    await disconnectDB(); // Disconnect from MongoDB gracefully
+    process.exit(0); // Exit the process gracefully
+});
+
+// Server Port
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ config/db.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+// Database connection URL (it will be pulled from .env file)
+const dbURI = process.env.MONGO_URI
+
+// Connection options
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+}
+
+// console.log(dbURI)
+
+/**
+ * Connect to MongoDB
+ */
+const connectDB = async () => {
+  try {
+    // Try to connect to the database using mongoose
+    await mongoose.connect(dbURI)
+    console.log('MongoDB connected...')
+  } catch (err) {
+    // Handle connection errors
+    console.error('Error connecting to MongoDB:', err)
+    process.exit(1) // Exit the process if DB connection fails
+  }
+}
+
+/**
+ * Close MongoDB connection gracefully
+ */
+const disconnectDB = async () => {
+  try {
+    await mongoose.disconnect()
+    console.log('MongoDB disconnected...')
+  } catch (err) {
+    console.error('Error disconnecting from MongoDB:', err)
+  }
+}
+
+module.exports = { connectDB, disconnectDB }

1b7539b8374c614985be7c67473d1abbb6285f89
Mchrispin <mchrispin14@gmail.com>
2025-02-11 10:25:38 +0200

    resolved push issues

--- src/main/java/com/school/library/librarymanagement/service/AuthenticationService.java
+++ src/main/java/com/school/library/librarymanagement/service/AuthenticationService.java
@@ -1,20 +1,17 @@
-// package
-
-// import com.school.library.librarymanagement.config.PasswordEncoderConfig;
-// import com.school.library.librarymanagement.repository.UserRepository;
-
-// @Service
-// public class AuthenticationService {
-
-//     @Autowired
-//     private UserRepository userRepository;
-
-//     @Autowired
-//     private PasswordEncoder passwordEncoder;
-
-//     public boolean authenticate(String contact, String rawPassword) {
-//         return userRepository.findByContact(contact)
-//             .map(user -> passwordEncoder.matches(rawPassword, user.getPassword()))
-//             .orElse(false);
-//     }
-// }
+/*
+ package
+ import com.school.library.librarymanagement.config.PasswordEncoderConfig;
+ import com.school.library.librarymanagement.repository.UserRepository;
+ @Service
+ public class AuthenticationService {
+     @Autowired
+     private UserRepository userRepository;
+     @Autowired
+     private PasswordEncoder passwordEncoder;
+     public boolean authenticate(String contact, String rawPassword) {
+         return userRepository.findByContact(contact)
+             .map(user -> passwordEncoder.matches(rawPassword, user.getPassword()))
+             .orElse(false);
+     }
+ }
+*/

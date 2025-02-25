bf11fcdbec6d0efd6b7e24f8cea92fbbff17098a
Mchrispin <mchrispin14@gmail.com>
2025-02-25 10:42:53 +0200

    resolving

--- config/origins.js
+++ config/origins.js
@@ -1,4 +1,4 @@
 export const allowedOrigins = [
     "http://localhost:5173", // For local development
-    "https://lib-backend-j3x3.onrender.com/api-docs/#"
+    "https://lib-backend-j3x3.onrender.com/api-docs"
   ]
